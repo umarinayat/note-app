afc433108d56f8eff5027fa5f8db2755d73c7c1a
= <=>
2022-08-30 15:07:37 +0500

    Notes

--- src/pages/Notes.jsx
+++ src/pages/Notes.jsx
@@ -1,7 +1,71 @@
 const Notes = () => {
   return (
     <>
-      <h2>Notes</h2>
+      <div className="flex flex-row flex-wrap gap-8 justify-center items-center p-16">
+        <div className="shadow-lg bg-gray-300 w-80 h-auto rounded-lg p-3 hover:translate-y-5 cursor-pointer hover:shadow-green-500">
+          <h2 className="text-black font-bold text-xl">Subject</h2>
+          <p className="text-gray-900 font-medium text-lg">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia
+            enim sequi explicabo quibusdam quis hic non at laborum, pariatur
+            vero ipsa unde, ipsum nam expedita. Aliquid voluptates delectus est
+            molestiae!
+          </p>
+        </div>
+        <div className="shadow-lg bg-gray-300 w-80 h-auto rounded-lg p-3 hover:translate-y-5 cursor-pointer hover:shadow-green-500">
+          <h2 className="text-black font-bold text-xl">Subject</h2>
+          <p className="text-gray-900 font-medium text-lg">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia
+            enim sequi explicabo quibusdam quis hic non at laborum, pariatur
+            vero ipsa unde, ipsum nam expedita. Aliquid voluptates delectus est
+            molestiae!
+          </p>
+        </div>
+        <div className="shadow-lg bg-gray-300 w-80 h-auto rounded-lg p-3 hover:translate-y-5 cursor-pointer hover:shadow-green-500">
+          <h2 className="text-black font-bold text-xl">Subject</h2>
+          <p className="text-gray-900 font-medium text-lg">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia
+            enim sequi explicabo quibusdam quis hic non at laborum, pariatur
+            vero ipsa unde, ipsum nam expedita. Aliquid voluptates delectus est
+            molestiae!
+          </p>
+        </div>
+        <div className="shadow-lg bg-gray-300 w-80 h-auto rounded-lg p-3 hover:translate-y-5 cursor-pointer hover:shadow-green-500">
+          <h2 className="text-black font-bold text-xl">Subject</h2>
+          <p className="text-gray-900 font-medium text-lg">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia
+            enim sequi explicabo quibusdam quis hic non at laborum, pariatur
+            vero ipsa unde, ipsum nam expedita. Aliquid voluptates delectus est
+            molestiae!
+          </p>
+        </div>
+        <div className="shadow-lg bg-gray-300 w-80 h-auto rounded-lg p-3 hover:translate-y-5 cursor-pointer hover:shadow-green-500">
+          <h2 className="text-black font-bold text-xl">Subject</h2>
+          <p className="text-gray-900 font-medium text-lg">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia
+            enim sequi explicabo quibusdam quis hic non at laborum, pariatur
+            vero ipsa unde, ipsum nam expedita. Aliquid voluptates delectus est
+            molestiae!
+          </p>
+        </div>
+        <div className="shadow-lg bg-gray-300 w-80 h-auto rounded-lg p-3 hover:translate-y-5 cursor-pointer hover:shadow-green-500">
+          <h2 className="text-black font-bold text-xl">Subject</h2>
+          <p className="text-gray-900 font-medium text-lg">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia
+            enim sequi explicabo quibusdam quis hic non at laborum, pariatur
+            vero ipsa unde, ipsum nam expedita. Aliquid voluptates delectus est
+            molestiae!
+          </p>
+        </div>
+        <div className="shadow-lg bg-gray-300 w-80 h-auto rounded-lg p-3 hover:translate-y-5 cursor-pointer hover:shadow-green-500">
+          <h2 className="text-black font-bold text-xl">Subject</h2>
+          <p className="text-gray-900 font-medium text-lg">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia
+            enim sequi explicabo quibusdam quis hic non at laborum, pariatur
+            vero ipsa unde, ipsum nam expedita. Aliquid voluptates delectus est
+            molestiae!
+          </p>
+        </div>
+      </div>
     </>
   );
 };
