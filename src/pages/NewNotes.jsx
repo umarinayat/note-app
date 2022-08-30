ee4275a81665e31b01b7fff5d3693b3476cb82ca
= <=>
2022-08-30 13:46:52 +0500

    First Commit

---
+++ src/pages/NewNotes.jsx
@@ -0,0 +1,9 @@
+const NewNotes = () => {
+  return (
+    <div>
+      <h2>New Notes</h2>
+    </div>
+  );
+};
+
+export default NewNotes;
---
+++ src/pages/Notes.jsx
@@ -0,0 +1,9 @@
+const Notes = () => {
+  return (
+    <>
+      <h2>Notes</h2>
+    </>
+  );
+};
+
+export default Notes;
